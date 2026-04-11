1682ae8a593ffee7e28c7aa564b68dc67ae391a8
ek0x <94327103+ek0x@users.noreply.github.com>
2026-04-11 11:14:10 +0300

    Update banka_takip_bot.py

--- banka_takip_bot.py
+++ banka_takip_bot.py
@@ -7,7 +7,7 @@ from pymongo import MongoClient
 import threading
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-BOT_TOKEN = os.getenv('8755846074:AAE0vZ3utPR9dPy1ng0ap3YBPQGnuJTNRt4')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 MONGODB_URI = os.getenv('mongodb+srv://emirhanksk:270325Ee.@telegram-bots.8l3uhpb.mongodb.net/?appName=telegram-bots')
 TURKIYE = pytz.timezone('Europe/Istanbul')
 
